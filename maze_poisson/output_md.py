3ff0a5d154b4427c104670d90e4062c9426364d0
Jayashree Narayan <102368145+JayashreeNarayan@users.noreply.github.com>
2024-12-13 19:22:39 +0530

    Update output_md.py

--- maze_poisson/output_md.py
+++ maze_poisson/output_md.py
@@ -1,6 +1,8 @@
 import atexit
 import os
 
+num_threads = int(os.environ.get("OMP_NUM_THREADS", 1))
+
 class OutputFiles:
     file_output_field = None
     file_output_performance = None
@@ -23,7 +25,7 @@ def generate_output_file(out_path, overwrite=True):
     return res
 
 
-def generate_output_files(grid, md_variables):
+def generate_output_files(grid):
     N = grid.N
     N_p = grid.N_p
     output_settings = grid.output_settings
@@ -39,44 +41,44 @@ def generate_output_files(grid, md_variables):
 
     if output_settings.print_field:
         os.makedirs(path, exist_ok=True)
-        output_field = os.path.join(path, 'field_N' + str(N) + '_N_p_'+str(N_p)+'.csv')
+        output_field = os.path.join(path, 'field_N' + str(N) +'_'+str(num_threads)+'.csv')
         output_files.file_output_field = generate_output_file(output_field)
         output_files.file_output_field.write("iter,x,MaZe\n")
 
     if output_settings.print_performance:
         os.makedirs(path, exist_ok=True)
-        output_performance = os.path.join(path, 'performance_tol'+str(md_variables.tol)+'.csv')
+        output_performance = os.path.join(path, 'performance_N' + str(N) +'_'+str(num_threads)+'.csv')
         output_files.file_output_performance = generate_output_file(output_performance)
         output_files.file_output_performance.write("iter,time,n_iters\n")
 
     if output_settings.print_iters:
         os.makedirs(path, exist_ok=True)
-        output_iters = os.path.join(path, 'iters_N' + str(N) + '_N_p_'+str(N_p)+'.csv')
+        output_iters = os.path.join(path, 'iters_N' + str(N) +'_'+str(num_threads)+'.csv')
         output_files.file_output_iters = generate_output_file(output_iters)
         output_files.file_output_iters.write("iter,max_sigma,norm\n")
 
     # prints only kinetic and non electrostatic potential
     if output_settings.print_energy:
         os.makedirs(path, exist_ok=True)
-        output_energy = os.path.join(path, 'energy_N' + str(N) + '_N_p_'+str(N_p)+'.csv')
+        output_energy = os.path.join(path, 'energy_N' + str(N) +'_'+str(num_threads)+'.csv')
         output_files.file_output_energy = generate_output_file(output_energy)
         output_files.file_output_energy.write("iter,K,V_notelec\n")
 
     if output_settings.print_temperature:
         os.makedirs(path, exist_ok=True)
-        output_temperature = os.path.join(path, 'temperature_N' + str(N) + '_N_p_'+str(N_p)+ '.csv')
+        output_temperature = os.path.join(path, 'temperature_N' + str(N) +'_'+str(num_threads)+ '.csv')
         output_files.file_output_temperature = generate_output_file(output_temperature)
         output_files.file_output_temperature.write("iter,T\n")
 
     if output_settings.print_solute:
         os.makedirs(path, exist_ok=True)
-        output_solute = os.path.join(path, 'solute_N' + str(N) + '_N_p_'+str(N_p)+'.csv')
+        output_solute = os.path.join(path, 'solute_N' + str(N) +'_'+str(num_threads)+'.csv')
         output_files.file_output_solute = generate_output_file(output_solute)
         output_files.file_output_solute.write("charge,iter,particle,x,y,z,vx,vy,vz,fx_elec,fy_elec,fz_elec\n")
 
     if output_settings.print_tot_force:
         os.makedirs(path, exist_ok=True)
-        output_tot_force = os.path.join(path, 'tot_force_N' + str(N) + '_N_p_'+str(N_p)+'.csv')
+        output_tot_force = os.path.join(path, 'tot_force_N' + str(N) +'_'+str(num_threads)+'.csv')
         output_files.file_output_tot_force = generate_output_file(output_tot_force)
         output_files.file_output_tot_force.write("iter,Fx,Fy,Fz\n")
 
